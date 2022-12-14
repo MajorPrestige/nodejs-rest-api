df87f022d2c3a70ef342dffe6be33a0bdee1cb56
Denys Dashkevych <denysdashkevych@gmail.com>
2022-12-14 20:56:28 +0200

    complete ex2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const ctrl = require('../../controllers/contacts');
-const ctrlWrapper = require('../../helpers/ctrlWrapper');
+const { ctrlWrapper } = require('../../helpers');
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(ctrl.getAll));
 router.get('/:contactId', ctrlWrapper(ctrl.getById));
 router.post('/', ctrlWrapper(ctrl.add));
-router.delete('/:contactId', ctrlWrapper(ctrl.remove));
 router.put('/:contactId', ctrlWrapper(ctrl.update));
+router.delete('/:contactId', ctrlWrapper(ctrl.remove));
 
 module.exports = router;
