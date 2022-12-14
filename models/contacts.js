6afac3e74c4d2bed112c236bd0b2e90b2e80901a
Denys Dashkevych <denysdashkevych@gmail.com>
2022-12-14 16:41:41 +0200

    added get function and get by id

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,61 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const contactId = String(id);
+  const desiredContact = contacts.find((el) => el.id === contactId);
 
-const updateContact = async (contactId, body) => {}
+  return desiredContact || null;
+};
+
+const removeContactById = async (id) => {
+  const contacts = await listContacts();
+  const contactId = String(id);
+  const indexToRemove = contacts.findIndex((el) => el.id === contactId);
+
+  if (indexToRemove === -1) {
+    return null;
+  }
+
+  const [contactToRemove] = contacts.splice(indexToRemove, 1);
+
+  updateContacts(contacts);
+
+  return contactToRemove;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+
+  const newContact = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+  updateContacts(contacts);
+
+  return newContact;
+};
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
+  removeContactById,
   addContact,
-  updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,47 @@
-const express = require('express')
+const express = require('express');
+const contacts = require('../../models/contacts');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      const error = new Error('Not Found');
+      error.status = 404;
+      throw error;
+    }
+    res.json(result);
+  } catch (error) {
+    const {status = 500, message = "Server error"} = error;
+    res.status(status).json({
+      message,
+    });
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
