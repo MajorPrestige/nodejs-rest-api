8568543f6725ec716d9879afccaf32a5b2be733d
Denys Dashkevych <denysdashkevych@gmail.com>
2022-12-23 18:52:23 +0200

    remove extra comments

--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-// const ctrl = require('../controllers/contacts');
-// const { ctrlWrapper } = require('../helpers');
 const ctrl = require('../controllers/contacts.controller');
 const { isValidId, isValidBody } = require('../middlewares');
 const {
@@ -17,12 +15,5 @@ router.put('/:contactId', isValidId, isValidBody(addSchema),ctrl.update);
 router.patch('/:contactId/favorite', isValidId, isValidBody(updateFavoriteSchema), ctrl.updateFavorite);
 router.delete('/:contactId', isValidId, ctrl.remove);
 
-
-// router.get('/', ctrlWrapper(ctrl.getAll));
-// router.get('/:contactId', ctrlWrapper(ctrl.getById));
-// router.post('/', ctrlWrapper(ctrl.add));
-// router.put('/:contactId', ctrlWrapper(ctrl.update));
-// router.patch('/:contactId/favorite', ctrlWrapper(ctrl.updateFavorite));
-// router.delete('/:contactId', ctrlWrapper(ctrl.remove));
 
 module.exports = router;
