0082ad998a94b7af98b6fb1e9f96fbe3fed1ab2b
Denys Dashkevych <denysdashkevych@gmail.com>
2023-01-04 17:30:06 +0200

    create upload middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const isValidId = require('./isValidId');
 const isValidBody = require('./isValidBody');
 const authenticate = require('./authenticate');
+const upload = require('./upload.js');
 
 module.exports = {
   isValidId,
   isValidBody,
   authenticate,
+  upload,
 };
